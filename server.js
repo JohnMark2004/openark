a5e840cf8adb08198da2c7ec071d306660c84adc
John Mark Lumada <jmlumada2004@gmail.com>
2025-09-20 14:39:49 +0800

    cors uodated

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "https://openark.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // if you need cookies/auth headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"), { index: false }));
 
